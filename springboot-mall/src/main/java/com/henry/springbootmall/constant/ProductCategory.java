e06a1215c147c6c9f8355b75662661a9464605e0
HenryShen2026 <lu811117@gmail.com>
2025-04-14 11:29:56 +0800

    商品功能 - Refactor category 使用 Enum 類型

---
+++ springboot-mall/src/main/java/com/henry/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,16 @@
+package com.henry.springbootmall.constant;
+
+/**
+ * ClassName: ProductCategory
+ * Package: com.henry.springbootmall.constant
+ * Description:
+ *
+ * @Author Henry Shen
+ * @Create 2025/4/14 上午 11:11
+ * @Version 1.0
+ */
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- springboot-mall/src/main/java/com/henry/springbootmall/model/Product.java
+++ springboot-mall/src/main/java/com/henry/springbootmall/model/Product.java
@@ -1,5 +1,6 @@
 package com.henry.springbootmall.model;
 
+import com.henry.springbootmall.constant.ProductCategory;
 import lombok.Data;
 
 import java.util.Date;
@@ -18,7 +19,7 @@ public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
