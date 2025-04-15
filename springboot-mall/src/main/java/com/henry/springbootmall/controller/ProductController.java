4e78af0edac0e4dfd4152ce0b3cf33ae8729d1b7
HenryShen2026 <lu811117@gmail.com>
2025-04-15 14:49:44 +0800

    商品功能 - 實作查詢商品列表 (分頁)

--- springboot-mall/src/main/java/com/henry/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/henry/springbootmall/controller/ProductController.java
@@ -6,9 +6,12 @@ import com.henry.springbootmall.dto.ProductRequest;
 import com.henry.springbootmall.model.Product;
 import com.henry.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +25,7 @@ import java.util.List;
  * @Create 2025/4/14 上午 10:49
  * @Version 1.0
  */
+@Validated
 @RestController
 public class ProductController {
 
@@ -35,13 +39,18 @@ public class ProductController {
             @RequestParam(required = false) String search,
             // 排序 sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "desc") String sort
+            @RequestParam(defaultValue = "desc") String sort,
+            // 分業 pagination
+            @RequestParam(defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
     ){
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
--- springboot-mall/src/main/java/com/henry/springbootmall/dto/ProductQueryParams.java
+++ springboot-mall/src/main/java/com/henry/springbootmall/dto/ProductQueryParams.java
@@ -19,5 +19,7 @@ public class ProductQueryParams {
     private String search;
     private String orderBy;
     private String sort;
+    private Integer limit;
+    private Integer offset;
 
 }
